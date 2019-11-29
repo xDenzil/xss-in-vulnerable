059a6334aabda9a993a7ef0c0b6a7b60d30733d4
Denzil Williams <denzilwilliams@Denzils-MacBook-Pro.local>
2019-11-29 17:42:13 -0500

    files

---
+++ sql_inserts.sql
@@ -0,0 +1,4 @@
+INSERT INTO `comments`(`name`, `message`) VALUES ('Denzil','Yo');
+INSERT INTO `comments`(`name`, `message`) VALUES ('Sashione','Hi Denzil');
+INSERT INTO `comments`(`name`, `message`) VALUES ('Denzil','You finish the work?');
+INSERT INTO `comments`(`name`, `message`) VALUES ('Sashione','working on it');
