a3b398184b46b34e4e89b3a567c5378719ef1818
Denzil Williams <denzilwilliams@Denzils-MacBook-Pro.local>
2019-11-29 17:44:17 -0500

    undo

--- sql_inserts.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `comments`(`name`, `message`) VALUES ('Denzil','Yo');
-INSERT INTO `comments`(`name`, `message`) VALUES ('Sashione','Hi Denzil');
-INSERT INTO `comments`(`name`, `message`) VALUES ('Denzil','You finish the work?');
-INSERT INTO `comments`(`name`, `message`) VALUES ('Sashione','working on it');
